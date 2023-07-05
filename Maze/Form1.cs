6556fa750bf25baf584dfe1260ba45827756f30c
saxonsgrammar <gramatikvlad1986@gmail.com>
2023-07-05 20:44:14 +0300

    first commit

---
+++ Maze/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        const int sizeX = 30;
+        const int sizeY = 30;
+        Labirint l;
+        public Form1()
+        {
+            InitializeComponent();
+            Options();
+            StartGame();
+        }
+        public void Options()
+        {
+            Text = "Здоровье - 100, медалей - 0";
+            BackColor = Color.FromArgb(255, 92, 118, 137);
+            this.ClientSize = new Size(sizeX * 16, sizeY * 16);
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        public void StartGame()
+        {
+            l = new Labirint(this, sizeX, sizeY);
+            l.Show();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) 
+            { 
+                if (l.maze[l.smileY, l.smileX + 1].type == MazeObject.MazeObjectType.HALL)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileX++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY, l.smileX + 1].type == MazeObject.MazeObjectType.HEALTH)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileX++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.health += 5;
+                    if (l.health > 100) l.health = 100;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY, l.smileX + 1].type == MazeObject.MazeObjectType.MEDAL)
+                {
+                    l.medals++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileX++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY, l.smileX + 1].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY + 1, l.smileX].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY - 1, l.smileX].type == MazeObject.MazeObjectType.ENEMY)
+                {
+                    l.health -= 20;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                Winning();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (l.maze[l.smileY, l.smileX - 1].type == MazeObject.MazeObjectType.HALL)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileX--;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                }
+                if (l.maze[l.smileY, l.smileX - 1].type == MazeObject.MazeObjectType.HEALTH)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileX--;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.health += 5;
+                    if (l.health > 100) l.health = 100;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY, l.smileX - 1].type == MazeObject.MazeObjectType.MEDAL)
+                {
+                    l.medals++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileX--;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY, l.smileX - 1].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY + 1, l.smileX].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY - 1, l.smileX].type == MazeObject.MazeObjectType.ENEMY)
+                {
+                    l.health -= 20;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                Winning();
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (l.maze[l.smileY - 1, l.smileX].type == MazeObject.MazeObjectType.HALL)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileY--;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                }
+                if (l.maze[l.smileY - 1, l.smileX].type == MazeObject.MazeObjectType.HEALTH)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileY--;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.health += 5;
+                    if (l.health > 100) l.health = 100;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY - 1, l.smileX].type == MazeObject.MazeObjectType.MEDAL)
+                {
+                    l.medals++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileY--;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY - 1, l.smileX].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY, l.smileX + 1].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY, l.smileX - 1].type == MazeObject.MazeObjectType.ENEMY)
+                {
+                    l.health -= 20;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                Winning();
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (l.maze[l.smileY + 1, l.smileX].type == MazeObject.MazeObjectType.HALL)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileY++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                }
+                if (l.maze[l.smileY + 1, l.smileX].type == MazeObject.MazeObjectType.HEALTH)
+                {
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileY++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.health += 5;
+                    if (l.health > 100) l.health = 100;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY + 1, l.smileX].type == MazeObject.MazeObjectType.MEDAL)
+                {
+                    l.medals++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[0];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    l.smileY++;
+                    l.maze[l.smileY, l.smileX].texture = MazeObject.images[4];
+                    l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                if (l.maze[l.smileY + 1, l.smileX].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY, l.smileX - 1].type == MazeObject.MazeObjectType.ENEMY || l.maze[l.smileY, l.smileX + 1].type == MazeObject.MazeObjectType.ENEMY)
+                {
+                    l.health -= 20;
+                    this.Text = "Здоровье - " + l.health + ", медалей - " + l.medals;
+                }
+                Winning();
+            }
+        }
+        public void Winning()
+        {
+            if (l.smileY == 27 && l.smileX == 28)
+            {
+                DialogResult result = MessageBox.Show("Вы победили - найден выход.", "", MessageBoxButtons.OK);
+                if (result == DialogResult.OK)
+                {
+                    Dispose();
+                }
+            }
+            //if (кол-во медалей) не догнал, как подсчитать
+            //{
+            //    DialogResult result = MessageBox.Show("Вы победили, собрав все медали.", "", MessageBoxButtons.OK);
+            //    if (result == DialogResult.OK)
+            //    {
+            //        Dispose();
+            //    }
+            //}
+            else if (l.health <= 0)
+            {
+                l.health = 0;
+                this.Text = "Здоровье: " + l.health + ", медалей: " + l.medals;
+                DialogResult result = MessageBox.Show("Вы проиграли - закончилось здоровье.", "", MessageBoxButtons.OK);
+                if (result == DialogResult.OK)
+                {
+                    Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Maze/Labirint.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Maze
+{
+    class Labirint
+    {
+        public int smileX { get; set; }
+        public int smileY { get; set; }
+        public int height; 
+        public int width;
+        public int health;
+        public int medals;
+        public MazeObject[,] maze;
+        public PictureBox[,] images;
+        public static Random r = new Random();
+        public Form parent;
+        public Labirint(Form parent, int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.parent = parent;
+            maze = new MazeObject[height, width];
+            images = new PictureBox[height, width];
+            smileX = 0;
+            smileY = 2;
+            health = 100;
+            medals = 0;
+            Generate();
+        }
+        private void Generate()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    MazeObject.MazeObjectType current = MazeObject.MazeObjectType.HALL;
+                    if (r.Next(5) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.WALL;
+                    }
+                    if (r.Next(250) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.MEDAL;
+                    }
+                    if (r.Next(250) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.ENEMY;
+                    }
+                    if (r.Next(150) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.HEALTH;
+                    }
+                    if (y == 0 || x == 0 || y == height - 1 | x == width - 1)
+                    {
+                        current = MazeObject.MazeObjectType.WALL;
+                    }
+                    if (x == smileX && y == smileY)
+                    {
+                        current = MazeObject.MazeObjectType.CHAR;
+                    }
+                    if (x == smileX + 1 && y == smileY || x == width - 1 && y == height - 3)
+                    {
+                        current = MazeObject.MazeObjectType.HALL;
+                    }
+                    maze[y, x] = new MazeObject(current);
+                    images[y, x] = new PictureBox();
+                    images[y, x].Location = new Point(x * maze[y, x].width, y * maze[y, x].height);
+                    images[y, x].Parent = parent;
+                    images[y, x].Width = maze[y, x].width;
+                    images[y, x].Height = maze[y, x].height;
+                    images[y, x].BackgroundImage = maze[y, x].texture;
+                    images[y, x].Visible = false;
+                }
+            }
+        }
+        public void Show()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    images[y, x].Visible = true;
+                }
+            }
+        }
+    }
+}
